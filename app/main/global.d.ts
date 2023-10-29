34d14dcafa6edfc8ffe822c01e95b2e4a39e3197
zengxiaoyu.ruler <zengxiaoyu.ruler@bytedance.com>
2023-10-29 12:07:07 +0800

    feat: compatible windows

--- app/main/global.d.ts
+++ app/main/global.d.ts
@@ -1,10 +1,4 @@
 declare module 'iconutil';
 declare module 'icns-lib';
 declare module 'xml2js';
-declare module 'ffi-napi'
-declare module 'ref-mapi'
-declare module 'ref-strcut-napi'
-declare module 'ref-wchar-napi'
 declare module 'icon-extractor'
-declare module 'win-api'
-declare module 'file-icon'
