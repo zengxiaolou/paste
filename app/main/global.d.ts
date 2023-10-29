d84783dffe9cfc2f5ec3d833c228ab1ef86874b2
zengxiaoyu.ruler <zengxiaoyu.ruler@bytedance.com>
2023-10-29 12:07:07 +0800

    feat: compatible windows

--- app/main/global.d.ts
+++ app/main/global.d.ts
@@ -2,6 +2,7 @@ declare module 'iconutil';
 declare module 'icns-lib';
 declare module 'xml2js';
 declare module 'ffi-napi'
-declare module'ref-mapi'
-declare module'ref-strcut-napi'
-declare module'ref-wchar-napi'
+declare module 'ref-mapi'
+declare module 'ref-strcut-napi'
+declare module 'ref-wchar-napi'
+declare module 'icon-extractor'
--- app/main/platform-utils/windows.ts
+++ app/main/platform-utils/windows.ts
@@ -1,65 +1,20 @@
-// import  struct  from 'ref-struct-napi';
-// import { wchar_t } from 'ref-wchar-napi';
-// import { ffi }  from 'ffi-napi';
-// import { ref } from 'ref-mapi'
-//
-// const SHFILEINFO = struct({
-//   'hIcon': 'long',
-//   'iIcon': 'int',
-//   'dwAttributes': 'uint32',
-//   'szDisplayName': [wchar_t, 260], // MAX_PATH
-//   'szTypeName': [wchar_t, 80]  // Adjust this value as per your requirement
-// });
-//
-// const shell32 = ffi.Library('Shell32.dll', {
-//   'SHGetFileInfoW': ['uint32', ['string', 'uint32', ref.refType(SHFILEINFO), 'uint32', 'uint32']]
-// });
-//
-// class WindowsUtils {
-//
-//   public getActiveApplicationName = (): Promise<string> => {
-//     return new Promise((resolve, reject) => {
-//       const handle = this.user32.GetForegroundWindow();
-//       if (!handle) {
-//         reject(new Error('Unable to get handle of active window.'));
-//         return;
-//       }
-//
-//       const buffer = Buffer.alloc(255 * wchar_t.size);
-//       const length = this.user32.GetWindowTextW(handle, buffer, buffer.length);
-//       if (length > 0) {
-//         resolve(buffer.toString('ucs2').replaceAll('\0', ''));
-//       } else {
-//         reject(new Error('Unable to get title of active window.'));
-//       }
-//     });
-//   };
-//
-//   public getIconForApplicationName = (appPath: string): Promise<string> => {
-//     return new Promise((resolve, reject) => {
-//       const shfi = new SHFILEINFO();
-//       const result = shell32.SHGetFileInfoW(
-//         appPath,
-//         0,
-//         shfi.ref(),
-//         shfi.constructor.size,
-//         0x1_00
-//       );
-//       console.log(result);
-//       // if (result) {
-//       //   // TODO: 处理 shfi.hIcon，例如将其转换为图像数据，并返回图像的路径或数据
-//       //   resolve(/* icon data or path */);
-//       // } else {
-//       //   reject(new Error('Unable to get icon.'));
-//       // }
-//     });
-//   };
-//
-//
-//   private user32 = ffi.Library('user32', {
-//     'GetForegroundWindow': ['long', []],
-//     'GetWindowTextW': ['int', ['long', wchar_t, 'int']]
-//   })
-// }
-//
-// export default WindowsUtils;
+import activeWin from 'active-win'
+
+class WindowsUtils {
+   public  getActiveApplicationName = async (): Promise<any> => {
+     const activeWindow = await activeWin();
+     if (activeWindow) {
+       console.log(`Active app name: ${activeWindow.owner.name}`);
+     } else {
+       console.log('No active window found');
+     }
+  };
+
+  public getIconForApplicationName = (appName: string): Promise<string> => {
+    return new Promise((resolve, reject) => {
+      resolve(appName)
+    });
+  };
+}
+
+export default WindowsUtils;
